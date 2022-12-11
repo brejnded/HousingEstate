2f50f2c2971e25c4918a60692dbf8d1eae8728d8
Matusko <matusnekoranec22@gmail.com>
2022-12-11 17:19:19 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate
+{
+    internal class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        public override string ToString()
+        {
+            return String.Format($"{name}\n{surname}\n{age}");
+        }
+
+
+    }
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HousingEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Janko_Kolenik = new Person("Janko", "Kolenik", 43 );
+            Console.WriteLine(Janko_Kolenik);
+        }
+    }
+}
